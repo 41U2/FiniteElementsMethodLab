23072428102ae6d67b2c3c55e836fe87a12f4dc2
Бондаренко Олег <oleg.bondarenko.2000@list.ru>
2022-12-17 15:51:14 +0300

    кастомная обработка ошибок PyQt добавлена, теперь когда приложение пдаает, то в консоль будет выводиться вся инфа об ерроре

--- UI/main.py
+++ UI/main.py
@@ -1,6 +1,13 @@
 import sys
 from PyQt5 import QtWidgets
 from UI.ui_main import *
+import traceback
+
+
+def except_hook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    print("Всё пошло по пизде, давай по новой !:", tb)
+
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()

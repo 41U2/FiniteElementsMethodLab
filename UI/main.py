b8292d9c85a6ae57b43cc311bebf55fa48cf3405
Бондаренко Олег <oleg.bondarenko.2000@list.ru>
2022-12-18 13:27:09 +0300

    Триангуляция запускается с UI и отрабатывает на тестовых данных Андрея

--- UI/main.py
+++ UI/main.py
@@ -1,13 +1,5 @@
 import sys
-from PyQt5 import QtWidgets
-from UI.ui_main import *
-import traceback
-
-
-def except_hook(exc_type, exc_value, exc_tb):
-    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
-    print("Всё пошло по пизде, давай по новой !:", tb)
-
+from UI.ui_controller import *
 
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
---
+++ UI/ui_controller.py
@@ -0,0 +1,60 @@
+from ui_main_page import *
+from Triangulation.triangulation import *
+import traceback
+import sys
+
+
+# касмтомные ерроры для вывода в консоль
+def except_hook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    print("Всё пошло по пизде, давай по новой !:", tb)
+
+
+sys.excepthook = except_hook
+
+
+class MainPage:
+
+    def __init__(self, main_page_input):
+        self.main_window = main_page_input
+        self.main_page = UIMainPage(self.main_window)
+        self.nx = None
+        self.ny = None
+        self.hx = None
+        self.hy = None
+        self.x0 = None
+        self.y0 = None
+        self.f = None
+        self.phi = None
+        self.psi = None
+        self.t = None
+
+        self.main_page.init_input_params_button_action(lambda: self.init_input_params())
+        self.main_page.create_plot_button_action(lambda: self.create_plot())
+
+        self.main_window.show()
+
+    def init_input_params(self):
+        self.nx = int(self.main_page.nx_input.toPlainText())
+        self.ny = int(self.main_page.ny_input.toPlainText())
+        self.hx = list(map(int, self.main_page.hx_input.toPlainText().split(',')))
+        self.hy = list(map(int, self.main_page.hy_input.toPlainText().split(',')))
+        self.x0 = int(self.main_page.x0_input.toPlainText())
+        self.y0 = int(self.main_page.y0_input.toPlainText())
+        self.f = self.main_page.f_input.toPlainText()
+        self.phi = self.main_page.phi_input.toPlainText()
+        self.psi = self.main_page.psi_input.toPlainText()
+        self.t = self.main_page.t_input.toPlainText()
+
+        print(self.nx, self.ny, self.hx, self.hy, self.x0, self.y0, self.f, self.f, self.phi, self.psi, 'input params are set')
+
+        self.invoke_triangulation()
+
+    def invoke_triangulation(self):
+        triangulation(self.x0, self.y0, self.nx, self.ny, self.hx, self.hy)
+        print('triangulation processed')
+
+    def create_plot(self):
+        print('plot created')
+
+

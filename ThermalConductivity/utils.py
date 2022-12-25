411c24aec9d9957b104fba6e9c80b0ba4ad546a8
aastashov <aastashov@atollis.com>
2022-12-25 17:55:56 +0300

    Реализован метод, составляющий матрицу демпфирования

--- ThermalConductivity/utils.py
+++ ThermalConductivity/utils.py
@@ -1,7 +1,7 @@
 from typing import List, Tuple
 
 
-def get_triangle_area(vertices: Tuple[List[float], List[float], List[float]]):
+def get_triangle_area(vertices: Tuple[List[float], List[float], List[float]]) -> float:
     x = 0
     y = 1
     return 0.5 * abs((vertices[1][x] - vertices[0][x]) * (vertices[2][y] - vertices[0][y]) -

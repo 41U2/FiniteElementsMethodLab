c9fbcdfa5c4150afa15e4a025285b8fb872dc146
aastashov <aastashov@atollis.com>
2022-12-25 17:10:09 +0300

    Добавлен метод вычисления площади треугольника по координатам его вершин

--- ThermalConductivity/utils.py
+++ ThermalConductivity/utils.py
@@ -1,1 +1,8 @@
-kek = 0
+from typing import List, Tuple
+
+
+def get_triangle_area(vertices: Tuple[List[float], List[float], List[float]]):
+    x = 0
+    y = 1
+    return 0.5 * abs((vertices[1][x] - vertices[0][x]) * (vertices[2][y] - vertices[0][y]) -
+                     (vertices[2][x] - vertices[0][x]) * (vertices[1][y] - vertices[0][y]))

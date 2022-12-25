a33c8ba88995a6549e217a9e35be780d42e5f37e
41U2 <alesha_u@mail.ru>
2022-12-25 23:07:25 +0300

    Убрал учет граничных условий у сурс матрицы

--- ThermalConductivity/matrices.py
+++ ThermalConductivity/matrices.py
@@ -10,27 +10,20 @@ from .utils import integrate_by_triangle
 def get_source_matrix(
         vertices: List[List[float]],
         triangle_indices: List[Tuple[int, int, int]],
-        source_function,
-        boundary_function,
-        is_boundary_vertices: List[bool]) -> Matrix:
+        source_function) -> Matrix:
     n_vertices = len(vertices)
-    n_triangles = len(triangle_indices)
     source_matrix = Matrix.zeros(n_vertices, 1)
-    for i_triangle in range(n_triangles):
+    for i, j, k in triangle_indices:
         triangle_vertices = (
-            vertices[triangle_indices[i_triangle][0]],
-            vertices[triangle_indices[i_triangle][1]],
-            vertices[triangle_indices[i_triangle][2]]
+            vertices[i],
+            vertices[j],
+            vertices[k]
         )
+        array = [i, j, k]
         for i_vertex in range(3):
             value = integrate_by_triangle(source_function, triangle_vertices, i_vertex)
-            current_value = source_matrix(triangle_indices[i_triangle][i_vertex], 0)
-            source_matrix.set_value(triangle_indices[i_triangle][i_vertex], 0, current_value + value)
-
-    for i_vertex in range(n_vertices):
-        if not is_boundary_vertices[i_vertex]:
-            continue
-        source_matrix.set_value(i_vertex, 0, boundary_function(vertices[i_vertex]))
+            current_value = source_matrix(array[i_vertex], 0)
+            source_matrix.set_value(array[i_vertex], 0, current_value + value)
 
     return source_matrix
 

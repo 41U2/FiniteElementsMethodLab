601bc48d6eff63675a5d4a34acf326e32584932c
aastashov <aastashov@atollis.com>
2022-12-26 16:15:52 +0300

    Автоформатирование, убрал точки с запятой в концах строк

--- CuthillMcKee/CuthillMcKee.py
+++ CuthillMcKee/CuthillMcKee.py
@@ -1,19 +1,19 @@
-#АЛГОРИТМ КАТХИЛЛА-МАККИ (ОБРАТНЫЙ) - вход: матрица смежности, Nx, Ny; выход: перенумерованный список вершин
+# АЛГОРИТМ КАТХИЛЛА-МАККИ (ОБРАТНЫЙ) - вход: матрица смежности, Nx, Ny; выход: перенумерованный список вершин
 def CuthillMcKee(AdjMatrix, Nx, Ny):
-    i=0;
-    NewOrder = [];
-    NewOrder_unique = []; # Список соответствия старых вершин новым. Старая вершина - индекс элемента, новая вершина - элемент по этому индексу
-    NewOrder.append(0);
-    NewOrder_unique.append(0);
+    i = 0
+    NewOrder = []
+    NewOrder_unique = []  # Список соответствия старых вершин новым. Старая вершина - индекс элемента, новая вершина - элемент по этому индексу
+    NewOrder.append(0)
+    NewOrder_unique.append(0)
 
     for i in NewOrder_unique:
-        if len(NewOrder_unique)>=Nx*Ny:
-            break;
+        if len(NewOrder_unique) >= Nx * Ny:
+            break
         for j in AdjMatrix[i]:
-            NewOrder.append(j);
+            NewOrder.append(j)
         for k in NewOrder:
             if k not in NewOrder_unique:
-                NewOrder_unique.append(k);
+                NewOrder_unique.append(k)
 
-    NewOrder_unique.reverse();
-    return NewOrder_unique;
+    NewOrder_unique.reverse()
+    return NewOrder_unique
--- CuthillMcKee/ReNum.py
+++ CuthillMcKee/ReNum.py
@@ -1,29 +1,29 @@
-#ПЕРЕНУМЕРАЦИЯ СПИСКА ВЕРШИН
+# ПЕРЕНУМЕРАЦИЯ СПИСКА ВЕРШИН
 def ReNum(NewOrder_unique, AdjMatrix, points, Nx, Ny):
-    i=0;
-    points_new = []; # Новый список вершин, полученный после перенумерации алгоритмом Катхилла-Макки
-    while i < Nx*Ny:
-        points_new.append(points[NewOrder_unique[i]]);
-        i+=1;
-    #print(points_new);
+    i = 0
+    points_new = []  # Новый список вершин, полученный после перенумерации алгоритмом Катхилла-Макки
+    while i < Nx * Ny:
+        points_new.append(points[NewOrder_unique[i]])
+        i += 1
+    # print(points_new);
 
-    #ЗАПОЛНЕНИЕ НОВОЙ МАТРИЦЫ СМЕЖНОСТИ
-    i=0;
-    j=0;
-    AdjMatrix_new = AdjMatrix; #Новая матрица смежности, полученная после перенумерации вершин
+    # ЗАПОЛНЕНИЕ НОВОЙ МАТРИЦЫ СМЕЖНОСТИ
+    i = 0
+    j = 0
+    AdjMatrix_new = AdjMatrix  # Новая матрица смежности, полученная после перенумерации вершин
     while i < len(AdjMatrix_new):
-        j=0;
+        j = 0
         while j < len(AdjMatrix_new[i]):
-                AdjMatrix_new[i][j] = NewOrder_unique[AdjMatrix_new[i][j]];
-                j+=1;
-        i+=1;
+            AdjMatrix_new[i][j] = NewOrder_unique[AdjMatrix_new[i][j]]
+            j += 1
+        i += 1
 
-    AdjMatrix_new1 = [];
-    i=0;
+    AdjMatrix_new1 = []
+    i = 0
     while i < len(NewOrder_unique):
-        AdjMatrix_new1.append(AdjMatrix_new[NewOrder_unique.index(i)]);
-        i+=1;
+        AdjMatrix_new1.append(AdjMatrix_new[NewOrder_unique.index(i)])
+        i += 1
     return [points_new, AdjMatrix_new1]
 
-#print(ReNum(CuthillMcKee(triangulation(x0, y0, Nx, Ny, hx, hy)[1]), triangulation(x0, y0, Nx, Ny, hx, hy)[1], triangulation(x0, y0, Nx, Ny, hx, hy)[0], Nx, Ny)[0], Nx, Ny); # Новый массив вершин
-#print(ReNum(CuthillMcKee(triangulation(x0, y0, Nx, Ny, hx, hy)[1]), triangulation(x0, y0, Nx, Ny, hx, hy)[1], triangulation(x0, y0, Nx, Ny, hx, hy)[0], Nx, Ny)[1], Nx, Ny); # Новая матрица смежности
+# print(ReNum(CuthillMcKee(triangulation(x0, y0, Nx, Ny, hx, hy)[1]), triangulation(x0, y0, Nx, Ny, hx, hy)[1], triangulation(x0, y0, Nx, Ny, hx, hy)[0], Nx, Ny)[0], Nx, Ny); # Новый массив вершин
+# print(ReNum(CuthillMcKee(triangulation(x0, y0, Nx, Ny, hx, hy)[1]), triangulation(x0, y0, Nx, Ny, hx, hy)[1], triangulation(x0, y0, Nx, Ny, hx, hy)[0], Nx, Ny)[1], Nx, Ny); # Новая матрица смежности
